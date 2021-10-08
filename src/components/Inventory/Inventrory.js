ee27f3b4d16806ea0491a2e109d64ce91e7131d5
muhammad-nazmul <muhammadnazmul246@gmail.com>
2021-10-08 12:26:27 +0600

    'addedNew'

---
+++ src/components/Inventory/Inventrory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventrory = () => {
+    return (
+        <div>
+            <h3>This is an Inventrory</h3>
+        </div>
+    );
+};
+
+export default Inventrory;
